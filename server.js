0aa11cf7a445ce08f6e801265ff251a56d9ec610
Loïc Ramefiarison <loicramefiarison@gmail.com>
2022-03-21 19:32:17 +0300

    change appname in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/lamba'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/lamba/index.html'));});
